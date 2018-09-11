3408f2d43156ed70f78653a505e8162520c387ce
Marcin Copik <mcopik@gmail.com>
2018-09-11 19:51:23 +0200

    Extend CMake support

--- code_builder/cmake.py
+++ code_builder/cmake.py
@@ -1,33 +1,37 @@
 import os.path, subprocess
 
-from os.path import join, exists
+from os.path import join, exists, isfile
 from os import listdir, mkdir
 from shutil import rmtree
+from glob import iglob
 
+def isCmakeProject(repo_dir):
+    return isfile( join(repo_dir, 'CMakeLists.txt') )
 
-class CMake:
+class CMakeProject:
 
     def __init__(self, repo_dir):
         self.repository_path = repo_dir
 
-
-    def build(self, force_update = False):
-        build_dir = self.repository_path + "_build"
+    def build(self, c_compiler, cxx_compiler, force_update = False):
+        self.build_dir = self.repository_path + "_build"
         # script is outside package directory
-        script_path = os.path.abspath(os.path.join(os.path.realpath(__file__), os.pardir))
-        script_path = os.path.abspath(os.path.join(script_path, os.pardir))
-        print(script_path)
-        c_compiler = join(script_path, "clang-wrapper")
-        cpp_compiler = join(script_path, "clang++-wrapper")
-        if not exists(build_dir):
-            mkdir(build_dir)
-        elif len(listdir(build_dir)) == 0 or force_update:
+        if not exists(self.build_dir):
+            mkdir(self.build_dir)
+        elif len(listdir(self.build_dir)) == 0 or force_update:
             c_compiler_opt = "-DCMAKE_C_COMPILER=" + c_compiler
-            cpp_compiler_opt = "-DCMAKE_CXX_COMPILER=" + cpp_compiler
+            cpp_compiler_opt = "-DCMAKE_CXX_COMPILER=" + cxx_compiler
             subprocess.run(["cmake", self.repository_path, c_compiler_opt, cpp_compiler_opt],
-                    cwd=build_dir
+                    cwd=self.build_dir
                     )
-        ret = subprocess.run(["cmake", "--build", "."], cwd=build_dir)
+        ret = subprocess.run(["cmake", "--build", "."], cwd=self.build_dir)
+
+    def generate_bitcodes(self, target_dir):
+        if not exists(target_dir):
+            mkdir(target_dir)
+        for file in iglob('{0}/**/*.ll'.format(self.build_dir), recursive=True):
+            filename = os.path.basename(file)
+            os.rename(file, os.path.join(target_dir, filename) )
 
     def clean(self):
         build_dir = self.repository_path + "_build"
--- code_builder/code_builder.py
+++ code_builder/code_builder.py
@@ -1,29 +1,56 @@
 
 import os.path, json
+from os import environ
 
 from git import Repo, GitCommandError
 
 from .cmake import CMake
 
-def import_projects(target_dir, specification):
+def import_projects(build_dir, target_dir, specification):
 
+    if not os.path.exists(build_dir):
+        os.mkdir(build_dir)
     if not os.path.exists(target_dir):
         os.mkdir(target_dir)
+
+    # override C++ compilers
+    if 'CC' in environ:
+        old_c_compiler = environ['CC']
+    else:
+        old_c_compiler = None
+    if 'CXX' in environ:
+        old_cxx_compiler = environ['CXX']
+    else:
+        old_cxx_compiler = None
+    script_path = os.path.abspath(os.path.join(os.path.realpath(__file__), os.pardir))
+    script_path = os.path.abspath(os.path.join(script_path, os.pardir))
+    c_compiler = os.path.join(script_path, "clang-wrapper")
+    cxx_compiler = os.path.join(script_path, "clang++-wrapper")
+    environ['CC'] = c_compiler
+    environ['CXX'] = cxx_compiler
+
     for repo, spec in specification.items():
 
         repository_path = spec['repository']
         project_name = repository_path[repository_path.rfind('/')+1 : repository_path.rfind('.git')]
         try:
-            print( "Clone project {0} from {1} to {2}".format(repo, repository_path, os.path.join(target_dir, project_name)))
-            cloned_repo = Repo.clone_from(repository_path, os.path.join(target_dir, project_name) )
+            print( "Clone project {0} from {1} to {2}".format(repo, repository_path, os.path.join(build_dir, project_name)))
+            cloned_repo = Repo.clone_from(repository_path, os.path.join(build_dir, project_name) )
         except GitCommandError:
-            cloned_repo = Repo( os.path.join(target_dir, project_name) )
+            cloned_repo = Repo( os.path.join(build_dir, project_name) )
 
         # classify repository
-        if os.path.isfile( os.path.join(cloned_repo.working_tree_dir, 'CMakeLists.txt') ):
-            print('Found Cmake')
-            cmake_repo = CMake(cloned_repo.working_tree_dir)
-            #cmake_repo.clean()
-            cmake_repo.build(force_update = True)
+        if isCmakeProject(cloned_repo.working_tree_dir):
+            cmake_repo = CMakeProject(cloned_repo.working_tree_dir)
+            cmake_repo.build(c_compiler=c_compiler, cxx_compiler = cxx_compiler, force_update = True)
+            cmake_repo.generate_bitcodes(os.path.join(target_dir, project_name))
 
+    if old_c_compiler != None:
+        environ['CC'] = old_c_compiler
+    else:
+        del environ['CC']
+    if old_cxx_compiler != None:
+        environ['CXX'] = old_cxx_compiler
+    else:
+        del environ['CXX']
 
