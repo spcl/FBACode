f7afdf4a7a60be0e37f7702d79d210b2d757624f
Marcin Copik <mcopik@gmail.com>
2018-09-13 00:31:50 +0200

    Fix incorrect looping

--- clang-wrapper-functions.sh
+++ clang-wrapper-functions.sh
@@ -6,6 +6,7 @@ function run_compilation() {
     # intercept compilation, ignore linking
     intercept_compilation=false
     ARGS=()
+    IR_FILES=()
     INPUT_ARGS=("$@")
     IGNORE_NEXT_ARG=false
     for i in $(seq 1 $#);
@@ -34,29 +35,35 @@ function run_compilation() {
             filename=$(basename -- "$var")
             filename="${filename%.*}"
             #echo "${dirname}/${filename}.ll"
+            IR_FILES+=("${dirname}/${filename}.ll")
             ARGS+=("${dirname}/${filename}.ll")
         fi
     done
     echo ${ARGS[@]}
-    echo $intercept_compilation
+    #echo $intercept_compilation
     if [ "$intercept_compilation" == true ]; then
         shopt -s nocasematch
-        for var in "${ARGS[@]}"
-        do
-            if [[ "$var" =~ ".cpp" ]] || [[ "$var" =~ ".c" ]] ; then
-                dirname=$(dirname -- "$var")
-                filename=$(basename -- "$var")
-                filename="${filename%.*}"
-                echo "Run LLVM generation with flags: ${ARGS[@]}"
-                ${LLVM_INSTALL_DIRECTORY}/bin/${compiler} "${@:2}"
-                ${LLVM_INSTALL_DIRECTORY}/bin/${compiler} -S -emit-llvm "${ARGS[@]}"
-                #${LLVM_INSTALL_DIRECTORY}/bin/clang "$@" 
-                #${LLVM_INSTALL_DIRECTORY}/bin/clang -S -emit-llvm "${ARGS[@]}"
-                #${LLVM_INSTALL_DIRECTORY}/bin/opt -load ${LLVM_TOOL_DIRECTORY}/libLLVMLoopStatistics.so -loop-statistics -loop-statistics-out-dir ${OUT_DIR} -loop-statistics-log-name "$filename" < "$filename.ll" > /dev/null
-            fi
-        done
+        echo "Run LLVM generation with flags: ${ARGS[@]}"
+        ${LLVM_INSTALL_DIRECTORY}/bin/${compiler} "${@:2}"
+        ${LLVM_INSTALL_DIRECTORY}/bin/${compiler} -S -emit-llvm "${ARGS[@]}"
+        #for var in "${ARGS[@]}"
+        #do
+        #    if [[ "$var" =~ ".cpp" ]] || [[ "$var" =~ ".c" ]] ; then
+        #        dirname=$(dirname -- "$var")
+        #        filename=$(basename -- "$var")
+        #        filename="${filename%.*}"
+        #        echo "Run LLVM generation with flags: ${ARGS[@]}"
+        #        ${LLVM_INSTALL_DIRECTORY}/bin/${compiler} "${@:2}"
+        #        ${LLVM_INSTALL_DIRECTORY}/bin/${compiler} -S -emit-llvm "${ARGS[@]}"
+        #        echo "${LLVM_INSTALL_DIRECTORY}/bin/${compiler} -S -emit-llvm "${ARGS[@]}" "${IR_FILES[@]}""
+        #        #${LLVM_INSTALL_DIRECTORY}/bin/clang "$@" 
+        #        #${LLVM_INSTALL_DIRECTORY}/bin/clang -S -emit-llvm "${ARGS[@]}"
+        #        #${LLVM_INSTALL_DIRECTORY}/bin/opt -load ${LLVM_TOOL_DIRECTORY}/libLLVMLoopStatistics.so -loop-statistics -loop-statistics-out-dir ${OUT_DIR} -loop-statistics-log-name "$filename" < "$filename.ll" > /dev/null
+        #    fi
+        #done
     else
-        echo "Run linking with flags: $@"
+        #echo "Run linking with flags: "${IR_FILES[@]}""
         "${LLVM_INSTALL_DIRECTORY}/bin/${compiler}" "${@:2}"
+        #${LLVM_INSTALL_DIRECTORY}/bin/llvm-as "${IR_FILES[@]}"
     fi
 }
