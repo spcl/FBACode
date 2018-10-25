cb316df4e17b9eda31ed0c5b0d9ea66170adc6f3
Marcin Copik <mcopik@gmail.com>
2018-10-26 00:51:50 +0200

    Fix subprocess implementation

--- code_builder/cmake.py
+++ code_builder/cmake.py
@@ -12,15 +12,15 @@ from sys import version_info
 def isCmakeProject(repo_dir):
     return isfile( join(repo_dir, 'CMakeLists.txt') )
 
-def run(command, dir, stdout, stderr):
+def run(command, cwd, stdout, stderr):
 
     # Python 3.5+ - subprocess.run
     # older - subprocess.call
-    version = version_info()
-    if version.major >= 3 and version.minor >= 5:
-        subprocess.run(command, cwd=dir, stdout = stdout, stderr = stderr)
+    # TODO: capture_output added in 3.7 - verify it works
+    if version_info.major >= 3 and version_info.minor >= 5:
+        return subprocess.run(command, cwd=cwd, stdout = stdout, stderr = stderr)
     else:
-        subprocess.call(command, cwd=dir, stdout = stdout, stderr = stderr)
+        return subprocess.call(command, cwd=cwd, stdout = stdout, stderr = stderr)
 
 class CMakeProject:
 
@@ -36,13 +36,15 @@ class CMakeProject:
         if len(listdir(self.build_dir)) == 0 or force_update:
             c_compiler_opt = "-DCMAKE_C_COMPILER=" + c_compiler
             cpp_compiler_opt = "-DCMAKE_CXX_COMPILER=" + cxx_compiler
+            cmd = ["cmake", self.repository_path, c_compiler_opt, cpp_compiler_opt]
             ret = run(
-                    ["cmake", self.repository_path, c_compiler_opt, cpp_compiler_opt],
+                    cmd,
                     cwd = self.build_dir,
                     stdout = PIPE,
                     stderr = PIPE
                     )
             if ret.returncode:
+                self.error_log.error('Failed CMake configure command: %s' % ' '.join(cmd))
                 self.error_log.error(ret.stderr.decode('utf-8'))
             else:
                 self.output_log.info('Configure %s to build in %s' % (self.repository_path, self.build_dir))
@@ -50,7 +52,6 @@ class CMakeProject:
         return False
 
     def build(self):
-        # TODO: capture_output added in 3.7 - verify it works
         ret = run(
                 ["cmake", "--build", "."],
                 cwd = self.build_dir,
