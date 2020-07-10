166a8c0851d82b8f4797177e93a39c5a38bf9a79
Marcin Copik <mcopik@gmail.com>
2020-07-10 15:17:15 +0200

    Add tools directory

---
+++ tools/build_docker_images.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import argparse
+import docker
+import json
+import os
+import shutil
+
+PROJECT_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), os.path.pardir)
+DOCKER_DIR = os.path.join(PROJECT_DIR, 'docker')
+REPOSITORY_NAME = 'mcopik/fbacode'
+
+images = {
+    'ubuntu-1804-clang-9': {
+        'dockerfile': 'Dockerfile.base'
+    }
+}
+
+client = docker.from_env()
+
+for img, definitions in images.items():
+    dockerfile = definitions['dockerfile']
+    client.images.build(
+        path=PROJECT_DIR,
+        dockerfile=os.path.join(DOCKER_DIR,dockerfile),
+        tag="{}:{}".format(REPOSITORY_NAME,img)
+    )
