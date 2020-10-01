65293cce7727ff4d5212cd7e552ba4e36a4eb398
Lukas Gygi <lukas.gygi@gmail.com>
2020-10-01 15:40:59 +0200

    cleaned up docker files

--- code_builder/wrappers/replace_compilers.sh
+++ code_builder/wrappers/replace_compilers.sh
@@ -3,11 +3,11 @@
 echo "Replace gcc, g++ & cpp by clang"
 
 for VERSION in "$@"; do
-    rm /usr/bin/g++-$VERSION /usr/bin/gcc-$VERSION /usr/bin/cpp-$VERSION
-    ln -s ${PWD}/clang++ /usr/bin/g++-$VERSION
-    ln -s ${PWD}/clang /usr/bin/gcc-$VERSION
-    ln -s ${PWD}/clang++ /usr/bin/cpp-$VERSION
-    ln -s ${PWD}/clang /usr/bin/cc-$VERSION
+    # rm /usr/bin/g++-$VERSION /usr/bin/gcc-$VERSION /usr/bin/cpp-$VERSION
+    ln -fs ${PWD}/clang++ /usr/bin/g++-$VERSION
+    ln -fs ${PWD}/clang /usr/bin/gcc-$VERSION
+    ln -fs ${PWD}/clang++ /usr/bin/cpp-$VERSION
+    ln -fs ${PWD}/clang /usr/bin/cc-$VERSION
 
     echo "Block the installation of new gcc version $VERSION"
     echo "gcc-$VERSION hold"|dpkg --set-selections
