a598f74c92294e8be8b1e81e64bec4321d098137
Lukas Gygi <lukas.gygi@gmail.com>
2020-11-04 23:55:28 +0100

    added travis as build system

---
+++ code_builder/build_systems/travis.py
@@ -0,0 +1,310 @@
+import shutil
+import subprocess
+import os
+import yaml
+import urllib.request
+import json
+
+from os.path import abspath, join, isfile, dirname, isdir
+from os import listdir, makedirs, mkdir, remove
+from subprocess import PIPE
+from shutil import rmtree
+from sys import version_info
+from re import search
+import pathlib
+
+from yaml.loader import FullLoader
+
+from .environment import get_c_compiler, get_cxx_compiler
+
+
+def run(command, cwd=None, stdout=None, stderr=None):
+
+    # Python 3.5+ - subprocess.run
+    # older - subprocess.call
+    # TODO: capture_output added in 3.7 - verify it works
+    print(" ".join(command))
+    if version_info.major >= 3 and version_info.minor >= 5:
+        return subprocess.run(command, cwd=cwd, stdout=stdout, stderr=stderr)
+    else:
+        return subprocess.call(command, cwd=cwd, stdout=stdout, stderr=stderr)
+
+
+class Context:
+    def __init__(self, cfg):
+        self.cfg = cfg
+
+    def set_loggers(self, out, err):
+        self.out_log = out
+        self.err_log = err
+
+
+class Project:
+    CONTAINER_NAME = "mcopik/fbacode:ubuntu-2004-travis"
+
+    def __init__(self, repo_dir, build_dir, idx, ctx):
+        self.repository_path = repo_dir
+        self.build_dir = build_dir
+        self.idx = idx
+        self.ctx = ctx
+        self.output_log = ctx.out_log
+        self.error_log = ctx.err_log
+    
+    def set_env_vars(self, var):
+        if not isinstance(var, str):
+            return False
+        env_vars = var.split(" ")
+        for env_var in env_vars:
+            env_var = env_var.split("=")
+            if len(env_var) >= 2:
+                os.environ[env_var[0]] = env_var[1]
+
+    def run_scripts(self, script_list):
+        if isinstance(script_list, str):
+            script_list = [script_list]
+        for cmd in script_list:
+            out = run(["bash", "-c", cmd], cwd=self.build_dir, stderr=subprocess.PIPE)
+            if out.returncode != 0:
+                self.error_log.print_error("running command \n{}\nfailed".format(cmd))
+                self.error_log.print_error(self.idx, str(out))
+                return False
+
+    def run_addons(self, addons):
+        apt = addons.get("apt")
+        # in case its just a string or list of strings
+        if apt and (isinstance(apt, str) or
+                    isinstance(apt, list) and all(isinstance(i, str) for i in apt)):
+            cmd = ["apt-get", "install", "-y", "--force-yes",
+                   "--no-install-recommends", apt]
+            out = run(cmd, cwd=self.build_dir, stderr=subprocess.PIPE)
+            if out.returncode != 0:
+                self.error_log.print_error("apt_packages install from .travis.yml failed")
+                self.error_log.print_error(self.idx, str(out))
+                return False
+        # in case it is more complicated
+        elif apt:
+            do_update = False
+            if "sources" in apt:
+                # add apt sources accoring to the yaml
+                do_update = True
+                # download apt source safelist file
+                safelist = None
+                url = "https://raw.githubusercontent.com/travis-ci/apt-source-safelist/master/ubuntu.json"
+                with urllib.request.urlopen(url) as resp:
+                    safelist = json.loads(resp.read().decode())
+                for source in apt["sources"]:
+                    key_url = None
+                    source_url = None
+                    if isinstance(source, str):
+                        # this should be in safelist
+                        safelist_entry = next(i for i in safelist if safelist["alias"] == source)
+                        key_url = safelist_entry.get("canonical_key_url", None)
+                        source_url = safelist_entry.get("sourceline")
+                    else:
+                        key_url = source.get("canonical_key_url", None)
+                        source_url = source.get("sourceline")
+                    if key_url:
+                        cmd = ["sh", "-c", "wget -q0 - {} | apt-key add -".format(key_url)]
+                        out = run(cmd, cwd=self.build_dir, stderr=subprocess.PIPE)
+                        if out.returncode != 0:
+                            self.error_log.print_error("adding key to repo failed")
+                            self.error_log.print_error(self.idx, str(out))
+                            return False
+                    if source_url is None:
+                        self.error_log.print_error("something went wrong with apt and travis")
+                        return False
+                    cmd = ["add-apt-repository", source_url]
+                    out = run(cmd, cwd=self.build_dir, stderr=subprocess.PIPE)
+                    if out.returncode != 0:
+                        self.error_log.print_error("adding repo failed")
+                        self.error_log.print_error(self.idx, str(out))
+                        return False
+            if apt.get("update") or do_update:
+                cmd = ["apt-get", "update"]
+                out = run(cmd, cwd=self.build_dir, stderr=subprocess.PIPE)
+                if out.returncode != 0:
+                    self.error_log.print_error("apt update from .travis.yml failed")
+                    self.error_log.print_error(self.idx, str(out))
+                    return False
+            if "packages" in apt:
+                if isinstance(apt["packages"], str):
+                    cmd = ["apt-get", "install", "-y", "--force-yes",
+                        "--no-install-recommends", apt["packages"]]
+                else: 
+                    # we have a list of packages
+                    cmd = ["apt-get", "install", "-y", "--force-yes",
+                        "--no-install-recommends"].extend(apt["packages"])
+                out = run(cmd, cwd=self.build_dir, stderr=subprocess.PIPE)
+                if out.returncode != 0:
+                    self.error_log.print_error("apt install from .travis.yml failed")
+                    self.error_log.print_error(self.idx, str(out))
+                    return False
+
+        apt_packages = addons.get("apt_packages")
+        if apt_packages:
+            cmd = ["apt-get", "install", "-y", "--force-yes",
+                   "--no-install-recommends", apt]
+            out = run(cmd, cwd=self.build_dir, stderr=subprocess.PIPE)
+            if out.returncode != 0:
+                self.error_log.print_error("apt_packages install from .travis.yml failed")
+                self.error_log.print_error(self.idx, str(out))
+                return False
+        # run the snap module
+        snaps = addons.get("snaps")
+        if snaps:
+
+            if isinstance(snaps, str):
+                cmd = ["snap", "install", snaps]
+                out = run(cmd, cwd=self.build_dir, stderr=subprocess.PIPE)
+                if out.returncode != 0:
+                    self.error_log.print_error("snap install from .travis.yml failed")
+                    self.error_log.print_error(self.idx, str(out))
+                    return False
+            else:
+                for snap in snaps:
+                    if isinstance(snap, str):
+                        cmd = ["snap", "install", snap]
+                    else:
+                        if "name" not in snap:
+                            self.error_log.print_error("invalid yaml file")
+                            return False
+                        cmd = ["snap", "install", snap["name"]]
+                        if "confinement" in snap:
+                            cmd.append("--{}".format(snap["confinement"]))
+                        if "channel" in snap:
+                            cmd.append("--channel={}".format(snap["channel"]))
+                    out = run(cmd, cwd=self.build_dir, stderr=subprocess.PIPE)
+                    if out.returncode != 0:
+                        self.error_log.print_error("snap install from .travis.yml failed")
+                        self.error_log.print_error(self.idx, str(out))
+                        return False
+        return True
+
+    def configure(self, force_update=True):
+        # open the .travis.yml file
+        if len(listdir(self.build_dir)) == 0 or force_update:
+            # clean build dir and copy source over
+            # we cant always build in separate directory from build
+            for f in listdir(self.build_dir):
+                if ".log" in f:
+                    continue
+                p = join(self.build_dir, f)
+                if isdir(p):
+                    shutil.rmtree(p)
+                else:
+                    remove(p)
+            cmd = ["bash", "-c", "shopt -s dotglob; cp -a {}/* {}".format(self.repository_path, self.build_dir)]
+            out = run(cmd, cwd=self.repository_path, stderr=subprocess.PIPE)
+            if out.returncode != 0:
+                self.error_log.print_error(self.idx, str(out))
+                return False
+        with open(join(self.build_dir, ".travis.yml"), 'r') as f:
+            yml = yaml.load(f, Loader=FullLoader)
+            print(yml)
+            self.yml = yml
+        # set global env vars specified in the yaml
+        for var in self.yml.get("env").get("global"):
+            self.set_env_vars(var)
+        # set default env variables
+        # https://docs.travis-ci.com/user/environment-variables/#default-environment-variables
+        os.environ["TRAVIS_BUILD_DIR"] = self.build_dir
+        os.environ["CI"] = "true"
+        os.environ["TRAVIS"] = "true"
+        c_compiler = get_c_compiler()
+        cxx_compiler = get_cxx_compiler()
+        os.environ["CXX"] = cxx_compiler
+        os.environ["CXX_FOR_BUILD"] = cxx_compiler
+        os.environ["CC"] = c_compiler
+        os.environ["CC_FOR_BUILD"] = c_compiler
+        # look for a good configuration of env or jobs or matrix:
+        
+        jobs = yml.get("jobs", yml.get("matrix", {})).get("include", None)
+        if jobs and isinstance(jobs, list):
+            # split this list into stages, since each stage need to be run afaik
+            travis_stages = [[]]
+            i = 0
+            for job in jobs:
+                if "stage" in job:
+                    if len(travis_stages[0] > 0):
+                        i += 1
+                        travis_stages.append([])
+                    travis_stages[i].append(job)
+                else:
+                    travis_stages[i].append(job)
+            for stage in travis_stages:
+                # try and filter out linux and clang jobs
+                linux_jobs = [i for i in stage if i.get("os") == "linux"]
+                if len(linux_jobs) > 0:
+                    stage = linux_jobs
+                clang_jobs = [i for i in stage if i.get("compiler") == "clang"]
+                if len(clang_jobs) > 0:
+                    stage = clang_jobs
+                # pick the first one of the matrix, idk how to handle it
+                if "env" in stage[0]:
+                    for var in stage[0]["env"]:
+                        self.set_env_vars(var)
+                if "addons" in stage[0]:
+                    if not self.run_addons(yml["addons"]):
+                        return False
+                if "before_install" in stage[0]:
+                    return self.run_scripts(yml["before_install"])
+                # run the install
+                if "install" in stage[0]:
+                    return self.run_scripts(yml["install"])
+                # run the before_script part
+                if "before_script" in stage[0]:
+                    return self.run_scripts(yml["before_script"])
+
+        # package addons
+        if "addons" in yml:
+            if not self.run_addons(yml["addons"]):
+                return False
+        #  TODO: pick a configuration from the env and rest of matrix
+        # cache components
+        # i dont think there is anything to do
+        # run the before_install script, if any
+        if "before_install" in yml:
+            return self.run_scripts(yml["before_install"])
+        # run the install
+        if "install" in yml:
+            return self.run_scripts(yml["install"])
+        # run the before_script part
+        if "before_script" in yml:
+            return self.run_scripts(yml["before_script"])
+
+    def build(self):
+        # run the script
+        if "script" in self.yml:
+            return self.run_scripts(self.yml["script"])
+        # before_cache would be run here, look into this
+
+        # maybe do after_success of after_failure?
+        # i think deploy parts can be ignored
+
+    def generate_bitcodes(self, target_dir):
+        for file in pathlib.Path(self.build_dir).glob("**/*.bc"):
+            # CMake file format: {build_dir}/../CMakeFiles/{dir}.dir/relative_bc_location
+            res = search(r"{}".format(self.build_dir), str(file))
+            local_path = str(file)[res.end(0) + 1:]
+            makedirs(join(target_dir, dirname(local_path)), exist_ok=True)
+            # os.rename does not work for target and destinations being
+            # on different filesystems
+            # we might operate on different volumes in Docker
+            shutil.move(file, join(target_dir, local_path))
+
+    def generate_ast(self, target_dir):
+        for file in pathlib.Path(self.build_dir).glob("**/*.ast"):
+            res = search(r"{}".format(self.build_dir), str(file))
+            local_path = str(file)[res.end(0) + 1:]
+            makedirs(join(target_dir, dirname(local_path)), exist_ok=True)
+            shutil.move(file, join(target_dir, local_path))
+        return True
+
+    def clean(self):
+        build_dir = self.repository_path + "_build"
+        rmtree(build_dir)
+        mkdir(build_dir)
+
+    @staticmethod
+    def recognize(repo_dir):
+        return isfile(join(repo_dir, ".travis.yml"))
---
+++ docker/ubuntu-2004-travis.dockerfile
@@ -0,0 +1,80 @@
+FROM ubuntu:20.04
+# set as env, we are noninteractive in the container too
+ENV DEBIAN_FRONTEND=noninteractive
+ENV SNAPCRAFT_SETUP_CORE=1
+
+ARG CLANG_VERSION=9
+
+ARG deps='software-properties-common curl gpg-agent gnupg' 
+ARG soft="python3 python3-pip cmake make clang-${CLANG_VERSION} libomp-${CLANG_VERSION}-dev clang++-${CLANG_VERSION} texinfo build-essential fakeroot devscripts automake autotools-dev wget snapd git ruby-full"
+RUN echo ${CLANG_VERSION}
+RUN apt-get update 
+RUN apt-get install -y ${deps} --no-install-recommends --force-yes
+RUN curl https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -
+RUN add-apt-repository "deb http://apt.llvm.org/focal/ llvm-toolchain-focal main"
+RUN apt-get update
+RUN apt-get install -y ${soft} --no-install-recommends --force-yes
+RUN apt-get purge -y --auto-remove ${DEPS}
+RUN ln -s /usr/bin/clang-${CLANG_VERSION} /usr/bin/clang
+RUN ln -s /usr/bin/clang++-${CLANG_VERSION} /usr/bin/clang++
+# install needed python modules
+RUN python3 -m pip install pyyaml
+
+ENV HOME_DIR /home/fba_code/
+ENV SRC_DIR ${HOME_DIR}/code
+ENV BUILD_DIR ${HOME_DIR}/build
+ENV BITCODES_DIR ${HOME_DIR}/bitcodes
+
+# set the environment variables for c/cxx
+# ENV CC ${HOME_DIR}/wrappers/clang
+# ENV CXX ${HOME_DIR}/wrappers/clang++
+
+# fixes for qmake
+# https://salsa.debian.org/lucas/collab-qa-tools/-/blob/master/modes/clang10
+# Force the configruation of qmake to workaround this issue:
+# https://clang.debian.net/status.php?version=9.0.1&key=FAILED_PARSE_DEFAULT
+
+RUN apt install --yes --no-install-recommends --force-yes qt5-qmake
+RUN cp /usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-clang/* /usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++/
+RUN ls -al /usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++/
+RUN cat /usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++/qmake.conf
+ENV QMAKESPEC=/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-clang/
+
+RUN sed -i -e "s|compare_problem(2,|compare_problem(0,|g" /usr/bin/dpkg-gensymbols
+RUN sed -i -e "s|compare_problem(1,|compare_problem(0,|g" /usr/bin/dpkg-gensymbols
+RUN grep "compare_problem(" /usr/bin/dpkg-gensymbols
+
+RUN apt search '^gcc-[0-9]*[.]*[0-9]*$' | grep -o '\bgcc[a-zA-Z0-9:_.-]*' |\
+  xargs -I {} echo "{}" hold | dpkg --set-selections
+
+
+RUN mkdir -p ${HOME_DIR}
+WORKDIR ${HOME_DIR}
+ADD docker/init.py init.py
+ADD code_builder/utils/ utils
+ADD code_builder/build_systems/ build_systems
+ADD code_builder/wrappers/ wrappers
+
+
+
+# https://clang.debian.net/
+# https://github.com/sylvestre/debian-clang/blob/master/clang-setup.sh
+# force, since apt installed compilers
+
+RUN ln -fs ${HOME_DIR}/wrappers/clang /usr/bin/cc\
+  && ln -fs ${HOME_DIR}/wrappers/clang++ /usr/bin/c++\
+  && ln -fs ${HOME_DIR}/wrappers/clang++ /usr/bin/cpp\
+  && ln -fs ${HOME_DIR}/wrappers/clang /usr/bin/gcc\
+  && ln -fs ${HOME_DIR}/wrappers/clang++ /usr/bin/g++
+
+# replace all version of gcc
+RUN cd ${HOME_DIR}/wrappers && ./replace_compilers.sh 4.6 4.7 4.8 4.9 5 6 7 8 9 10
+
+
+
+# Check if gcc, g++ & cpp are actually clang
+RUN gcc --version|grep clang > /dev/null || exit 1
+RUN g++ --version|grep clang > /dev/null || exit 1
+RUN cpp --version|grep clang > /dev/null || exit 1
+
+ENTRYPOINT ["python3", "-u", "init.py", "input.json"]
