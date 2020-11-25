a870d99bb3b510aff91e58843bb639ea6bff8ea8
Lukas Gygi <lukas.gygi@gmail.com>
2020-11-25 21:17:56 +0100

    handle case of no ci system

---
+++ code_builder/ci_systems/None.py
@@ -0,0 +1,23 @@
+
+# what to do when there is no recognized ci system?
+class Context:
+    def __init__(self, cfg):
+        self.cfg = cfg
+
+    def set_loggers(self, out, err):
+        self.out_log = out
+        self.err_log = err
+
+
+class CiSystem:
+    def __init__(self, repo_dir, build_dir, idx, ctx, project):
+        self.repository_path = repo_dir
+        self.build_dir = build_dir
+        self.idx = idx
+        self.ctx = ctx
+        self.output_log = ctx.out_log
+        self.error_log = ctx.err_log
+        self.project = project
+
+    def install(self):
+        pass
