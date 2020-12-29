ea2f566d5597e5194835ff75b9e973fee92f836b
Lukas Gygi <lukas.gygi@gmail.com>
2020-12-29 22:53:40 +0100

    parallelized fetcher for debian

--- code_builder/fetcher.py
+++ code_builder/fetcher.py
@@ -2,6 +2,8 @@ from requests import get
 from time import time, sleep
 from string import ascii_lowercase
 from random import shuffle
+from concurrent.futures import ThreadPoolExecutor
+import concurrent.futures
 
 
 def maximum_repos(cfg):
@@ -151,7 +153,7 @@ class GithubFetcher:
 
 
 class DebianFetcher:
-    def __init__(self, cfg, out_log, error_log):
+    def __init__(self, cfg, out_log, error_log, thread_count=10):
         self.cfg = cfg
         self.out_log = out_log
         self.error_log = error_log
@@ -166,47 +168,69 @@ class DebianFetcher:
             + list(ascii_lowercase[12:])
         )
         self.suite = cfg["debian"]["suite"]
+        self.thread_count = thread_count
 
     def fetch(self, max_repos=None):
         # fetch results to self.results
         if not max_repos:
             max_repos = -1
+        self.max_repos = max_repos
+        print(max_repos)
         repo_count = 0
         self.results = []
         self.out_log.set_counter(max_repos)
         self.error_log.set_counter(max_repos)
         start = time()
-        # comment for predictable results
-        shuffle(self.prefixes)
-        for i, prefix in enumerate(self.prefixes):
-            self.out_log.info("fetching pkgs with prefix {}".format(prefix))
-            prefix_response = get(
-                # XXX: change from /copyright/api correct?
-                "https://sources.debian.org/api/prefix/{}/?suite={}".format(
-                    prefix, self.suite
-                )
+        # lets get random pkgs from the whole list -> more randomness
+        all_pkgs = get(
+            "https://sources.debian.org/api/list/?suite={}".format(self.suite)
+        )
+        if all_pkgs.status_code != 200:
+            # hmmm fuck
+            self.error_log.error(
+                "error fetching {}, code {}".format(all_pkgs.url, all_pkgs.status_code)
             )
-            if prefix_response.status_code != 200:
-                # hmmm fuck
-                self.error_log.error(
-                    "error fetching {}, code {}".format(
-                        prefix_response.url, prefix_response.status_code
+            return False
+        pkg_list = all_pkgs.json()["packages"]
+        shuffle(pkg_list)
+        futures = []
+        index = 0
+        print("loaded all debian pkgs, length: {}".format(len(pkg_list)))
+        with ThreadPoolExecutor(max_workers=self.thread_count) as executor:
+            # start thread_count workers
+            for index in range(0, self.thread_count):
+                print(
+                    "[{}/{}] fetched, trying index {}: {}".format(
+                        repo_count, max_repos, index, pkg_list[index]["name"]
                     )
                 )
-                return False
-            data = prefix_response.json()
-            # comment for predictable results
-            shuffle(data["packages"])
-            for pkg in data["packages"]:
-                # parallelize this
-                if self.package_info(pkg):
-                    repo_count += 1
-                    self.out_log.next()
-                    self.error_log.next()
-                if repo_count == max_repos:
-                    break
-            if repo_count == max_repos:
-                break
+                future = executor.submit(self.package_info, pkg_list[index]["name"])
+                futures.append(future)
+            incomplete_futures = True
+            # when one finishes, increment counter and add a new one to the queue
+            while incomplete_futures:
+                done, incomplete_futures = concurrent.futures.wait(
+                    futures, return_when=concurrent.futures.FIRST_COMPLETED
+                )
+                for future in done:
+                    result = future.result()
+                    futures.remove(future)
+                    if repo_count < min(max_repos, len(pkg_list)):
+                        if result is not False:
+                            repo_count += 1
+                            self.results.append(result)
+                        print(
+                            "[{}/{}] fetched, trying index {}: {}".format(
+                                repo_count, max_repos, index, pkg_list[index]["name"]
+                            )
+                        )
+                        new_future = executor.submit(
+                            self.package_info, pkg_list[index]["name"]
+                        )
+                        futures.append(new_future)
+                        index += 1
+
+        print("done!")
         end = time()
         self.out_log.info(
             "got {} c/c++ packages in {} seconds!".format(repo_count, end - start)
@@ -217,7 +241,9 @@ class DebianFetcher:
         if not self.results:
             return False
         processed_results = {}
-        for pkg in self.results:
+        for i, pkg in enumerate(self.results):
+            if i == self.max_repos:
+                break
             processed_results[pkg["name"]] = {
                 "type": "debian",
                 "version": pkg["version"],
@@ -235,40 +261,37 @@ class DebianFetcher:
     def update(self, existing_repo):
         pass
 
-    def package_info(self, pkg):
+    def package_info(self, name):
         # get the version number for this package
-        self.out_log.info("fetching info for {}".format(pkg["name"]))
+        # TODO: check for suite
+        # self.out_log.info("fetching info for {}".format(name))
         response = get(
-            "https://sources.debian.org/api/src/{}/?suite={}".format(
-                pkg["name"], self.suite
-            )
+            "https://sources.debian.org/api/src/{}/?suite={}".format(name, self.suite)
         )
         if response.status_code != 200:
             self.error_log.error(
                 "error fetching pkg versions for {}, code {}".format(
-                    pkg["name"], response.status_code
+                    name, response.status_code
                 )
             )
             return False
         # first version should be correct because we specify suite in url
-        if (
-            not isinstance(response.json()["versions"], list)
-            or len(response.json()["versions"]) == 0
-        ):
+        if not isinstance(response.json().get("versions"), list):
             print("weird json response:")
             print(response.json())
+            return False
+        if len(response.json()["versions"]) == 0:
+            # print("is not in {} suite".format(self.suite))
             return False
         version = response.json()["versions"][0]["version"]
         # get more info for package and version
         response = get(
-            "https://sources.debian.org/api/info/package/{}/{}".format(
-                pkg["name"], version
-            )
+            "https://sources.debian.org/api/info/package/{}/{}".format(name, version)
         )
         if response.status_code != 200:
             self.error_log.error(
                 "error fetching pkg info for {}, code {}".format(
-                    pkg["name"], response.status_code
+                    name, response.status_code
                 )
             )
             return False
@@ -283,24 +306,38 @@ class DebianFetcher:
         # if response.json()["pkg_infos"]["sloc"][0][0] in c_names:
         # XXX: ignore packages with more than 1mil LOC (just for testing)
         if any(c_sloc) and int(response.json()["pkg_infos"]["sloc"][0][1]) < 100000:
-            self.out_log.info("contains c/c++!")
-            self.results.append(
-                {
-                    # pkg["name"]: {
-                    "version": version,
-                    "name": pkg["name"],
-                    "sloc": response.json()["pkg_infos"]["sloc"],
-                    "suite": self.suite,
-                    "vcs_browser": response.json()["pkg_infos"]["vcs_browser"]
-                    if "vcs_browser" in response.json()["pkg_infos"]
-                    else None,
-                    "vcs_type": response.json()["pkg_infos"]["vcs_type"]
-                    if "vcs_type" in response.json()["pkg_infos"]
-                    else None
-                    # }
-                }
-            )
-            return True
+            # self.out_log.info("contains c/c++!")
+            # self.results.append(
+            #     {
+            #         # name: {
+            #         "version": version,
+            #         "name": name,
+            #         "sloc": response.json()["pkg_infos"]["sloc"],
+            #         "suite": self.suite,
+            #         "vcs_browser": response.json()["pkg_infos"]["vcs_browser"]
+            #         if "vcs_browser" in response.json()["pkg_infos"]
+            #         else None,
+            #         "vcs_type": response.json()["pkg_infos"]["vcs_type"]
+            #         if "vcs_type" in response.json()["pkg_infos"]
+            #         else None
+            #         # }
+            #     }
+            # )
+            # return True
+            return {
+                # name: {
+                "version": version,
+                "name": name,
+                "sloc": response.json()["pkg_infos"]["sloc"],
+                "suite": self.suite,
+                "vcs_browser": response.json()["pkg_infos"]["vcs_browser"]
+                if "vcs_browser" in response.json()["pkg_infos"]
+                else None,
+                "vcs_type": response.json()["pkg_infos"]["vcs_type"]
+                if "vcs_type" in response.json()["pkg_infos"]
+                else None
+                # }
+            }
         return False
 
 
